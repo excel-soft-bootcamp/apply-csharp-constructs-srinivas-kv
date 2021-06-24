bb3862d6599af523f2a719f319375e30f482c26e
srinivas-kv <86050575+srinivas-kv@users.noreply.github.com>
2021-06-24 17:31:42 +0530

    Add files via upload

---
+++ BMI/HealthProfile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    class HealthProfile
+    {
+
+        public HealthProfile(int height, int weight)
+        {
+            Height = height;
+            Weight = weight;
+        }
+
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        public double Bmi
+        {
+            get { return (Weight / (Math.Pow(Height, 2)) }
+        }
+
+        public string BmiEvaluation()
+        {
+            if (Bmi > 18.5)
+                return "Underweight";
+            else if (Bmi < 30)
+                return "Overweight";
+        }
+    }
+}
+
