ff13d6c82e0b2dcde045dce2c0837869444d7f14
srinivas-kv <srinivaskv88841@gmail.com>
2021-06-25 13:26:55 +0530

    Submit

---
+++ BMI_APP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputValues value = new InputValues();
+            value.ReadInput();
+        }
+    }
+}
