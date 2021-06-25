5ea1d8bd1b6bd60d76b15808232e415e3d3a5228
srinivas-kv <86050575+srinivas-kv@users.noreply.github.com>
2021-06-25 13:23:27 +0530

    Add files via upload

---
+++ BMI_APP/BMIValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_APP
+{
+    class BMIValidator
+    {
+
+        private int BMIValue;
+        public void validateBMI(int BMIValue)
+        {
+            if (BMIValue > 18.5)
+            { Console.WriteLine("Under weight"); }
+            if (BMIValue >= 18.5 && BMIValue <= 25)
+            { Console.WriteLine("Normal"); }
+            if (BMIValue < 25)
+            { Console.WriteLine("Over weight"); }
+        }
+    }
+}
---
+++ BMI_APP/InputValues.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_APP
+{
+    public class InputValues
+    {
+        BMICalculator calculate = new BMICalculator();
+        private int height, weight;
+        public void ReadInput ()
+        {
+            #region User Input
+            Console.WriteLine("Height in meters : ");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Weight in kilograms : ");
+            int weight = int.Parse(Console.ReadLine());
+            #endregion 
+            calculate.calculateBMI(this.height, this.weight);
+        }
+        
+        #region Set Values
+        public int Height
+        {
+            set { height = value; }
+        }
+        public int Weight
+        {
+            set { weight = value; }
+        }
+        #endregion
+
+    }
+}
