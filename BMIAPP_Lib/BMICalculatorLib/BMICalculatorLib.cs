347a4c3814bc71e41542c61b12be54dd66d44be5
srinivas-kv <srinivaskv88841@gmail.com>
2021-07-06 15:58:37 +0530

    commit

---
+++ BMIAPP_Lib/BMICalculatorLib/BMICalculatorLib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculatorLib
+{
+    public static class BMICalculatorLib
+    {
+        public static float BMI(float weight, float height)
+        {
+            return (weight / height);
+        }
+    }
+}
---
+++ BMIAPP_Lib/BMI_Application/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            float heightValue = PersonDataLib.PersonDataLib.SetHeight();
+            float weightValue = PersonDataLib.PersonDataLib.SetWeight();
+            float Bmi = BMICalculatorLib.BMICalculatorLib.BMI(weightValue, heightValue);
+            string consoleMessage = BMIValidatorLib.BMIValidatorLib.Validate(Bmi);
+            ConsoleDisplayLib.ConsoleDisplayLib.Display(consoleMessage);
+        }
+    }
+}
