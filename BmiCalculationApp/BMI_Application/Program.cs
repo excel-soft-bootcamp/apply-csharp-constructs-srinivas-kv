57cdd065ba52c9b91bb1a854209efef90bac3121
hemanth111p <72683647+hemanth111p@users.noreply.github.com>
2021-07-06 23:26:49 +0530

    updated

---
+++ BmiCalculationApp/BMI_Application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            float heightValue = PersonDataLib.PersonDataType.GetHeight();
+            float weightValue = PersonDataLib.PersonDataType.GetWeight();
+
+            float bmiValue = BMICalculatorLib.BMICalculatorType.Calculator(weightValue, heightValue);
+
+            string consoleMessage = BMIValidatorLib.BMIValidatorType.Validate(bmiValue);
+
+            ConsoleDisplayLib.ConsoleDisplayType.Display(consoleMessage);
+        }
+    }
+}
---
+++ BmiCalculationApp/PersonDataLib/PersonDataType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataLib
+{
+    public static class PersonDataType
+    {
+        
+        public static float GetHeight()
+        {
+          return ConsoleUserInputlib.ConsoleUserInputType.SetHeight();
+           
+        }
+        public static float GetWeight()
+        {
+         return  ConsoleUserInputlib.ConsoleUserInputType.SetWeight(); 
+        }
+    }
+}
